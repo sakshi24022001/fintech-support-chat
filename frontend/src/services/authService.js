168a1401da1a05fa07522b2f388706992ea06dca
sakshi24022001 <sakshibalodiya2402@gmail.com>
2026-02-09 02:27:45 +0530

    update URL

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // Live URL
+  baseURL: "https://fintech-support-chat.onrender.com",
+  
+//  Local url
+  // baseURL: "http://localhost:5000/api",
 });
 
 export const loginUser = (data) =>
